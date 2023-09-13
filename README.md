3103f004e2b8b0b645db8a347b4d6c737d658286
AdityaHitachi <aditya.gadekar@hitachivantara.com>
2023-09-13 11:41:41 +0530

    [HCICD-462] : Notifications to Teams

--- README.md
+++ README.md
@@ -1,1 +1,93 @@
-# teams-notify-action
+# teams-notify-action 🐝
+
+This Teams Notify action aims to send messages to a  Microsoft Teams channel to help users keep track of all the defined steps in a workflow, along with additional workflow details.
+
+This action explicitly tracks some specific steps within pull request and merge workflows as per [**Honeycomb**](https://hv-eng.atlassian.net/wiki/spaces/MCI/pages/30469586968/CICD+Pipeline) 🐝 initiative.
+
+- For the pull request workflow, this action keeps track of whether the **[Build, Unit Test, and SonarQube]** steps were skipped, executed, or encountered failures. 
+
+- For the merge request workflow, this action monitors and records the status of the **[Build, Unit Test, SonarQube, and Blackduck]** steps, tracking whether they were skipped, executed successfully, or experienced failures.
+
+To track Builds, SonarQube analysis, Unit tests, and Black Duck scans within our team's notifications, we need to include unique identifiers when triggering GitHub Actions for each specific step. 
+
+Below are id values for each  step :
+1) Build step: Build
+2) Unit Test step: Unit_Test
+3) Sonarqube Scan: Sonarqube
+4) Blackduck Scan: Blackduck
+
+### Example 
+```
+# Assign unique IDs to Build, Unit Test, SonarQube, and BlackDuck steps as follows:
+
+- name: Build
+  id: Build
+  run: echo " Run Build Cmds"
+  shell: bash
+
+- name: Unit Test
+  id: Unit_Test
+  run: echo " Run Unit Step Cmd"
+  shell: bash
+
+- name: Sonarqube Scan
+  id: Sonarqube
+
+- name: Blackduck Scan
+  id: Blackduck
+```
+**Note - If we do not provide these ID values to their respective steps, all steps will be automatically marked as skipped**
+
+- ✅ OK: Indicates that the step ran successfully without any errors or issues.
+
+- 💥 FAIL: Indicates that the step ran but failed due to some error encountered during its execution.
+
+- ❌ SKIP: Indicates that the step was not executed or was intentionally skipped for some reason.
+
+### Here are examples of how to use this action in your workflow:
+#### Calling teams-notify-action in workflow
+```
+# Calling teams-notify-action action in workflow
+- name: Teams Notification
+  if: always()
+  uses: hv-actions/teams-notify-action@master
+  with:
+    steps_json: '${{ toJson(steps) }}'    
+    teams_Webhook_Url: '${{ secrets.TEAMS_WEBHOOK_URL }}'
+
+```
+#### To create a hyperlink in a team message, you can pass the following value to the Teams action:
+
+```
+- name: Teams Notification
+  if: always()
+  uses: hv-actions/teams-notify-action@HCICD-462
+  with:
+    steps_json: '${{ toJson(steps) }}'
+    teams_Webhook_Url: '${{ secrets.TEAMS_WEBHOOK_URL }}' 
+    sonar_Host_Url: '${{env.SONAR_HOST_URL}}'              # Server URL for SonarQube, e.g.,https://sonar.orl.eng.hitachivantara.com
+    sonar_Project_Key: '${{env.SONAR_PROJECT_KEY}}'        # Project key for SonarQube, e.g.,multi-module-tester
+    blackDuck_Server_Url: '${{env.BLACKDUCK_SERVER_URL}}'  # Serve URL for Blackduck, e.g.,https://orion.orl.eng.hitachivantara.com
+    blackDuck_ProjId: '${{env.BLACKDUCK_PROJ_ID}}'         # Blackduck Project ID e.g.,06641177-59f0-4310-833c-8c04e6bd84f4
+    blackDuck_VersionId: '${{env.BLACKDUCK_VERSION_ID}}'   # Blackduck Version ID e.g.,f9378655-8282-44d3-a49b-bff0ebf2a032
+    unit_test_url: 'e.g.,https://test.test'                # Unit test report link, Currently it's only work with Honeycomb CI/CD pipeline
+```
+**Note - The hyperlink will appear in the message only if the step is executed and valid inputs for the hyperlink are passed to the action**
+### Here are examples of how the messages will appear in the team channel:
+
+This message includes the following sections
+- Details: Contains a hyperlink providing information about file changes.
+- Branch: Displays the name of the branch that triggered the workflow.
+- Event: Indicates the type of event that initiated the workflow (e.g., push, pull_request).
+- Workflow Steps: Provides information on the status of all workflow steps.
+- Footer: The username of the user that triggered the initial workflow run.
+- Color Line: A green line indicates a successful workflow, while a red line indicates a failed workflow.
+
+#### For Pull_request
+![Alt text](./Images/pull_request_workflow.png)
+
+#### For Merge request
+![Alt text](./Images/merge_workflow.png)
+
+#### For Failed workflow
+![Alt text](./Images/failed_workflow.png)
---
+++ src/index.py
@@ -0,0 +1,208 @@
+import os
+import json
+import yaml
+import requests
+import re
+import requests
+
+#Define Empty section input
+sections_input = ""
+#---
+
+#Fetching json for step details
+steps_input = os.environ.get('STEPS_JSON')
+webhook_input = os.environ.get('TEAMS_WEBHOOK_URL')
+job_status_input = os.environ.get('JOB_STATUS')
+# ---
+
+#Create message footer
+github_event_actor = os.environ.get('GITHUB_ACTOR')
+event_actor_icon_url = f"https://github.com/{github_event_actor}.png?size=20"
+response = requests.get(event_actor_icon_url, allow_redirects=True)
+# Get the final redirected URL (the URL after all redirects)
+github_user_icon = response.url
+# ---
+
+# Set github event name "push, pull_request.. etc "
+github_event_name = os.environ.get('GITHUB_EVENT_NAME')
+# ---
+
+#Set Commit message details 
+github_event_head_url = os.environ.get('GITHUB_EVENT_HEAD_COMMIT_URL')
+github_event_head_commit_message = os.environ.get('GITHUB_EVENT_HEAD_COMMIT_MESSAGE')
+github_server_url = os.environ.get('GITHUB_SERVER_URL')
+github_repository = os.environ.get('GITHUB_REPOSITORY')
+github_event_pull_request_number = os.environ.get('GITHUB_PULL_REQUEST_NUMBER')
+github_branch = os.environ.get('GITHUB_BRANCH_REF')
+github_run_id = os.environ.get('GITHUB_RUN_ID')
+# ---
+
+#sonar login details
+sonar_host_url = os.environ.get('SONAR_HOST_URL')
+sonar_project_key = os.environ.get('SONAR_PROJECT_KEY')
+# ---
+
+#Blackduck details
+blackduck_server_url = os.environ.get('BLACKDUCK_SERVER_URL')
+blackduck_proj_id = os.environ.get('BLACKDUCK_PROJ_ID')
+blackduck_version_id = os.environ.get('BLACKDUCK_VERSION_ID')
+# ---
+
+#Set Workflow details Url
+workflow_url = f"{github_server_url}/{github_repository}/actions/runs/{github_run_id}"
+
+#Set Commit message for notification
+if github_event_name not in ["pull_request", "pull_request_target"]:
+    if not github_event_head_url:
+        details = "Details"
+    else:
+        details = github_event_head_url
+
+    commit = github_event_head_commit_message
+    if not commit:
+        commit = f"''Triggered by {github_event_name}''"
+    else:
+        commit = f"''{commit[:27]}...''"
+elif github_event_name == "pull_request" or github_event_name == "pull_request_target":
+    details = f"{github_server_url}/{github_repository}/pull/{github_event_pull_request_number}"
+    commit = f"''Triggered by {github_event_name}''"
+
+
+# This code checks a JSON file to ensure that data coming in as string input is enclosed within double quotes. 
+# It does this by using regular expressions to identify and add double quotes to unquoted values that appear within JSON key-value pairs.
+def add_double_quotes(match):
+    return f'"{match.group(0)}"'
+
+steps_input = re.sub(r'(?<!")(\w+)(?=:)', add_double_quotes, steps_input)
+steps_input = re.sub(r'(?<=: )(\w+)(?=[,\n])', add_double_quotes, steps_input)
+# --
+
+#Tracking all steps for Push event and other than push
+def step_track(steps_input):
+    if 'Build' not in steps_input:
+        steps_input['Build'] = {'outputs': {}, 'outcome': 'skipped', 'conclusion': 'skipped'}
+    if 'Sonarqube' not in steps_input:
+        steps_input['Sonarqube'] = {'outputs': {}, 'outcome': 'skipped', 'conclusion': 'skipped'}
+    if 'Unit_Test' not in steps_input:
+        steps_input['Unit_Test'] = {'outputs': {}, 'outcome': 'skipped', 'conclusion': 'skipped'}
+        
+    # Define desired sequence based on event type
+    if github_event_name != "pull_request" and github_event_name != "pull_request_target":
+        if 'Blackduck' not in steps_input:
+            steps_input['Blackduck'] = {'outputs': {}, 'outcome': 'skipped', 'conclusion': 'skipped'}
+        desired_sequence = ['Build', 'Unit_Test', 'Sonarqube', 'Blackduck']
+    else:
+        desired_sequence = ['Build', 'Unit_Test', 'Sonarqube']
+
+    # Formating steps in desired sequence
+    desired_sequence_data = {key: steps_input[key] for key in desired_sequence if key in steps_input}
+
+    #Adding remaining step at end other than desired sequence
+    for key, value in steps_input.items():
+        if key not in desired_sequence:
+            desired_sequence_data[key] = value
+
+    return desired_sequence_data
+
+#Sonar Url generation
+if sonar_host_url and sonar_project_key:
+    if github_event_name == "pull_request" or github_event_name == "pull_request_target":
+        generate_sonarqube_url = f"{sonar_host_url}/dashboard?id={sonar_project_key}&pullRequest={github_event_pull_request_number}"
+    else:
+        generate_sonarqube_url = f"{sonar_host_url}/dashboard?id={sonar_project_key}&branch={github_branch}"
+else:
+    generate_sonarqube_url = ""
+
+# Blackduck url generation
+if github_event_name != "pull_request" and github_event_name != "pull_request_target":
+    if blackduck_server_url != "":
+        genereted_blackduck_url = f"{blackduck_server_url}/api/projects/{blackduck_proj_id}/versions/{blackduck_version_id}/components"
+    else:
+        genereted_blackduck_url = ""
+else:
+    genereted_blackduck_url = ""
+
+# Unit Test Url link from honeycomb
+generated_unit_test_url = os.environ.get('UNIT_TEST_URL')
+
+# Function to Replace link in message
+def replace_values_with_links(data,sonarqube_url,unit_test_url,blackduck_url): 
+    for item in data:
+        if item['name'] == 'Unit_Test' and item['value'] != '❌ SKIP' and unit_test_url:
+            item['value'] = f'<a href="{unit_test_url}">{item["value"]}</a>'
+        elif item['name'] == 'Sonarqube' and item['value'] != '❌ SKIP' and sonarqube_url:
+            item['value'] = f'<a href="{sonarqube_url}">{item["value"]}</a>'
+        elif item['name'] == 'Blackduck' and item['value'] != '❌ SKIP' and blackduck_url:
+            item['value'] = f'<a href="{blackduck_url}">{item["value"]}</a>'
+    return data
+
+#Creating teams message Structure using json_payload
+json_payload = {
+    "@type": "MessageCard",
+    "@context": "http://schema.org/extensions",
+    "summary": "Honeycomb Teams Notification",
+    "sections": [],
+    "potentialAction": []
+}
+
+# Adding green and red line based on job status in messsage Box
+theme_color = {"themeColor": "00FF00" if job_status_input == "success" else "FF0000"}
+
+json_payload.update(theme_color)
+
+emoji_map = {
+    'success': '✅ OK',
+    'failure': '💥 FAIL',
+    'skipped': '❌ SKIP'
+}
+
+# Set message content
+if sections_input:
+    json_payload["sections"] = yaml.safe_load(sections_input)
+else:
+    json_payload["sections"].extend([
+        {"text": f"<a href='{details}'>Details</a>"},
+        {"text": f"<h3>Commit message : {commit}</h3>"},
+        {"text": f"<h3>Branch : {github_branch}</h3>"},
+        {"text": f"Event: <a href='{workflow_url}'>{github_event_name}"}
+    
+    ])
+
+    if steps_input:
+        final_dict= {}
+        steps_input = step_track(json.loads(steps_input))
+        for key, value in steps_input.items():
+            for k, v in value.items():
+                if k == 'outcome':
+                    value[k] = emoji_map.get(v, v)
+                    final_dict[key] = value[k]
+                                   
+        fact_list = [{'name': key, 'value': value} for key, value in final_dict.items() if not any(char.isdigit() for char in key)]
+        json_payload["sections"].append({"text": "<h2><strong>Workflow Steps</strong></h2>"})
+        
+        #Updating fact_list with link
+        final_fact_list = replace_values_with_links(fact_list,generate_sonarqube_url,generated_unit_test_url,genereted_blackduck_url)
+        
+        #Setting Steps Names and status icon
+        json_payload["sections"].append({"facts": final_fact_list})
+        
+        #Setting footer of message
+        json_payload["sections"].append({"text": f'<img src="{github_user_icon}"> by {github_event_actor}'})
+
+def teams_message_send(webhook_input, json_payload):
+    headers = {'Content-Type': 'application/json'}
+    
+    try:
+        response = requests.post(webhook_input, json=json_payload, headers=headers)
+        response.raise_for_status()  # Raise an exception for 4xx and 5xx HTTP status codes
+        print('Request sent successfully')
+        print('Response:', response.text)
+    except requests.exceptions.RequestException as e:
+        print('Error sending the request:', e)
+
+#Calling funtion to send message
+teams_message_send(webhook_input, json_payload)
+
+# You can set the JSON payload as an output variable if needed
+json_payload_str = json.dumps(json_payload, indent=2)
+print('jsonPayload:', json_payload_str)
